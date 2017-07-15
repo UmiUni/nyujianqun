7df9c168d2f884ab93dcf4560705fc334b2cec16
Chaoran Wang <superchaoran@gmail.com>
2017-07-15 07:59:55 +0000

    tiantian

--- tiantian.py
+++ tiantian.py
@@ -131,9 +131,9 @@ def tuling_reply(msg):
     elif "10" in msgText:
         pullMembersMore(msg, u'线上KTV', CurUserName)
         sleep(0.5)
-    else:
-        itchat.send_msg(vT, CurUserName)
-        sleep(0.5)
+    sleep(0.5)
+    itchat.send_msg(vT, CurUserName)
+    sleep(0.5)
 
 def pullMembersMore(msg, chatroomName, CurUserName):
     cur_chatrooms = itchat.search_chatrooms(name=chatroomName)
