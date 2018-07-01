920de1730ecfa36ab9c42040c22e566215c6461a
Chaoran Wang <superchaoran@gmail.com>
2018-07-01 18:37:27 +0000

    adding nyu

--- tiantian.py
+++ tiantian.py
@@ -20,6 +20,7 @@ settings.init()
 def add_friend(msg):
     itchat.add_friend(**msg['Text'])
     itchat.send_msg(settings.vT, msg['RecommendInfo']['UserName'])
+    itchat.send_msg(settings.v18, msg['RecommendInfo']['UserName'])
 
 @itchat.msg_register(itchat.content.TEXT)
 def tuling_reply(msg):
@@ -32,24 +33,10 @@ def tuling_reply(msg):
 #send group invite msg according to digits
 def sendGroupInviteMsg(msg,CurUserName):
   msgText = msg['Text']
-  x = re.findall(r'\d+', msgText)
-  #print x
-  if(len(x) >0):
-    y= int(x[0])
-    if(y>=0 and y<=15):
-      if msg['FromUserName'] not in settings.admins:
-        if(preventAbuseTalking(CurUserName)):
-          return
-      pullMembersMore(msg, settings.chatGroups[y], CurUserName)
-      sleep(0.5)
-    elif(y==99):
-      advertiseQR(CurUserName)
-    elif(y==100):
-      pullMembersMore(msg, settings.chatGroups[16], CurUserName)
-      sleep(0.5)
   itchat.send_msg(settings.vT, CurUserName)
   sleep(0.5)
-  msgText = msg['Text']
+  itchat.send_msg(settings.v18, CurUserName)
+  sleep(0.5)
 
 #if group chat msg contains kick ads, start kicking logic
 @itchat.msg_register(TEXT, isGroupChat=True)
